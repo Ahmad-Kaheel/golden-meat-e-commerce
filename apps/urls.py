698e4c8b0206ca8fcb0f107ab1df941ed9f56ccc
modar-d-dwery <majddw312@gmail.com>
2024-08-11 23:11:23 +0300

    apps urls file updated with address and profile end points

--- apps/urls.py
+++ apps/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("register/", UserRegisterationAPIView.as_view(), name="user_register"),
     path("login/", UserLoginAPIView.as_view(), name="user_login"),
     path("user/", include("customer.urls", namespace="users")),
+    path("address/", include("address.urls", namespace="addresses")),
+    path("catalogue/", include("catalogue.urls", namespace="catalogues")),
 ]

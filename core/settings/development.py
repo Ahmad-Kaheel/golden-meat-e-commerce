02fbd0d9253bb3ef6d8bc6a2856fedfe1ae9a767
modar-d-dwery <majddw312@gmail.com>
2024-08-14 09:33:42 +0300

    update development settings

--- core/settings/development.py
+++ core/settings/development.py
@@ -30,10 +30,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.postgresql",
-        'NAME': config("DB_NAME", default=""),
-        'USER': config("DB_USER", default=""),
-        'PASSWORD': config("DB_PASSWORD", default=""),
-        'HOST': config("DB_HOST", default="localhost"),
-        'PORT': config("DB_PORT", default="5432"),
+        'DB_NAME': 'mydatabase',
+        'DB_USERNAME': 'myuser',
+        'DB_PASSWORD': 'mypassword',
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }

ce516e344d01551d3ae02265b70d0487557c2356
modar-d-dwery <majddw312@gmail.com>
2024-08-14 09:25:47 +0300

    update development settings

--- core/settings/development.py
+++ core/settings/development.py
@@ -25,3 +25,15 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'example@example.com'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': config("DB_NAME", default=""),
+        'USER': config("DB_USER", default=""),
+        'PASSWORD': config("DB_PASSWORD", default=""),
+        'HOST': config("DB_HOST", default="localhost"),
+        'PORT': config("DB_PORT", default="5432"),
+    }
+}

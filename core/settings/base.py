118a58b8123a1289864e70e6a3255aea1a8baf95
modar-d-dwery <majddw312@gmail.com>
2024-08-24 13:58:52 +0300

    update docker

--- core/settings/base.py
+++ core/settings/base.py
@@ -117,16 +117,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mydatabase', 
-#         'USER': 'myuser',
-#         'PASSWORD': 'mypassword',
-#         'HOST': "db",
-#         'PORT': "5432",
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase', 
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': "db",
+        'PORT': "5432",
+    }
+}
 
 # DB_ENGINE = config("DB_ENGINE", default="django.db.backends.sqlite3")
 # DB_NAME = config("DB_NAME", default=os.path.join(PARENT_DIR, "db.sqlite3"))
